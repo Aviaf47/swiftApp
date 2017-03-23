818a21e3310ba531a087e32925c3e2c362c04d2f
user120718 <user120718@NY213.local>
2017-03-22 19:00:34 -0700

    my first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by user120718 on 3/22/17.
+//  Copyright © 2017 Aviaf Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func Yo(_ sender: Any) {
+        
+    //this is a comment text
+        
+        theLabel.text = "Buttons are Cool"
+    }
+    @IBAction func buttonTappe(_ sender: AnyObject) {
+     tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "You've pushed me too many times!"
+        }
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
